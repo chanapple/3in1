4da143351f8827e0dd7b1cca40518283afcd4399
chana <chanapple0703@gmail.com>
2024-07-09 15:31:00 +0900

    게임 컨트롤러 파일 중복 경로 제거

--- game/game.controller.js
+++ game/game.controller.js
@@ -3,10 +3,11 @@ import path from 'path';
 
 /** 환경설정:상수 경로 설정 */
 const __dirname = path.resolve();
+const __gamedirname = path.join(__dirname, '/game');
 
 /** 메인 모듈 */
 function viewIndex(req, res){
-    res.sendFile(path.join(__dirname, '/game/index.html'));
+    res.sendFile(path.join(__gamedirname, '/index.html'));
 }
 
 /** 모듈 내보내기 */
