7b8e8fc61baee12a1728dcd66305cecd9304495b
yws3806 <yws3806@gmail.com>
2024-07-28 15:05:55 +0900

    주석 달았음

--- game/js/main.js
+++ game/js/main.js
@@ -7,12 +7,13 @@ import {
 } from "/game/phaser/phaser.esm.min.js";
 
 import playScene from "/game/js/playScene.js";
-
+/** 게임 내 전체 월드 크기 */
 const worldWidth = 8000;
 const worldHeight = 6400;
+/** 사용자에게 보이는 화면 크기 */
 const displayWidth = 1200;
 const displayHeight = 800;
-
+/**  */
 const config = {
 	type: AUTO,
 	scale: {
@@ -21,6 +22,7 @@ const config = {
 		width: displayWidth,
 		height: displayHeight
 	},
+	/** arcde라는 물리 엔진 추가, 중력을 300으로 설정 */
 	physics: {
 		default: "arcade",
 		arcade: {
